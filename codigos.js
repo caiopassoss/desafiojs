22c4be1f7863f47111c259c6aa94d4145e1d1546
caiopassoss <130685233+caiopassoss@users.noreply.github.com>
2023-05-19 09:01:43 -0300

    Add files via upload

---
+++ codigos.js
@@ -0,0 +1,20 @@
+
+function ativarNome(){
+    let nome = 'CP tbest';
+    console.log(nome)
+    document.write(nome)
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 2;
+
+
+    let c = a+b;
+
+    console.log(c)
+    document.write(c)
+    
+
+}
